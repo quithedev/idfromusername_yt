bd04ba162cb8a382f29efc44bd1195a240c15695
qui <_qui@tuta.io>
2024-10-20 14:16:52 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests,re,sys
+from bs4 import BeautifulSoup
+
+def get_channel_id(username):
+    url = f"https://www.youtube.com/@{username}"
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"HTTP hatası: {e}")
+        return None
+    
+    page_content = response.text
+    
+    external_id_match = re.search(r'data-channel-external-id="(UC[0-9A-Za-z_-]+)"', page_content)
+    if external_id_match:
+        return external_id_match.group(1)
+
+    external_id_match = re.search(r'"externalId":"(UC[0-9A-Za-z_-]+)"', page_content)
+    if external_id_match:
+        return external_id_match.group(1)
+
+    return None
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    username = username = input("Username (Without @): ")
+
+channel_id = get_channel_id(username)
+
+if channel_id:
+    print(channel_id)
+    sys.exit(0)
+else:
+    print("User doesn't exist.")
+    sys.exit(1)
